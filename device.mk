d91809b870d40ce0fce42426808e563e490bf783
Jaideep Sharma <jaidshar@codeaurora.org>
2019-09-05 16:15:40 +0530

    vince: Update sound trigger HIDL version to 2.2
    Change-Id: Idb683714c52bfdf17ea709c6f47d312582042190

--- device.mk
+++ device.mk
@@ -67,7 +67,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
-    android.hardware.soundtrigger@2.1-impl
+    android.hardware.soundtrigger@2.2-impl
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \

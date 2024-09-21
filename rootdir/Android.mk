45331f14202036631ac66baff2dd36b338091cb8
ALprjkt <168145100+ALprjkt@users.noreply.github.com>
2024-09-22 03:13:15 +0700

    vince: Decommonized tree
    Signed-off-by: ALprjkt <168145100+ALprjkt@users.noreply.github.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -5,12 +5,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom
 LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-ifeq ($(AB_OTA_UPDATER), true)
-LOCAL_SRC_FILES    := etc/fstab_AB.qcom
-else
+LOCAL_MODULE_CLASS := LOC
 LOCAL_SRC_FILES    := etc/fstab.qcom
-endif
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
@@ -61,6 +57,14 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.qcom.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.recovery.qcom.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 # Configuration scripts

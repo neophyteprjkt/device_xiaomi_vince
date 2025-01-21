8e7ecdabbab5af6987bd18efb917872396f04f4a
AL <168145100+k4ngcaribug@users.noreply.github.com>
2025-01-21 21:57:43 +0700

    vince: Update removepackages

--- removepackages/Android.mk
+++ removepackages/Android.mk
@@ -5,26 +5,28 @@ LOCAL_MODULE := removepackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
+	AudioFX \
 	Aperture \
 	arcore \
 	AICorePrebuilt \
 	AiWallpapers \
 	AndroidAutoStubPrebuilt \
 	Camera2 \
-        DevicePersonalizationPrebuiltPixel2020 \
+        DuckDuckGo \
         Flash \
 	GoogleTTS \
+	Gallery2 \
+	Jellyfish \
+	Jelly \
 	Maps \
-	Photos \
 	PixelThemesStub \
 	PixelThemesStub2022_and_newer \
 	PixelWallpapers2023 \
 	PixelLiveWallpaperPrebuilt \
 	SnapCamera \
 	SafetyHubPrebuilt \
+        SimpleGallery \
 	TurboPrebuilt \
-	Velvet \
-	WellbeingPrebuilt \
 	YouTube
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := platform

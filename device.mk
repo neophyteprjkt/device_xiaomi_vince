1967ca11dc32119310765b172fd0edd7a72e779f
k4ngcaribug <168145100+k4ngcaribug@users.noreply.github.com>
2025-02-26 01:30:53 +0000

    Revert "vince: Drop thermal 1.0"
    This reverts commit 73b6f0e10d1262a9aa5b78b78cfaae504055d195.

--- device.mk
+++ device.mk
@@ -425,6 +425,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/textclassifier/textclassifier.universal.model:$(TARGET_COPY_OUT_SYSTEM)/etc/textclassifier/textclassifier.universal.model
 
 # Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
+    android.hardware.thermal@1.0.vendor
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 

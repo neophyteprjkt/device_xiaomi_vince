e52e348ffd6c2c4e5ce6c89511f462376802ba30
AL <168145100+k4ngcaribug@users.noreply.github.com>
2025-02-26 16:35:34 +0700

    vince: vendorsetup: fix miss branch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 git clone --depth=1 https://github.com/vince-labs/vendor_xiaomi_vince -b 13 vendor/xiaomi/vince
 git clone --depth=1 https://github.com/vince-labs/kernel_xiaomi_vince -b main kernel/xiaomi/vince
 git clone --depth=1 https://github.com/k4ngcaribug/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
-git clone --depth=1 https://github.com/k4ngcaribug/vendor_extra -b 10 vendor/extra
+git clone --depth=1 https://github.com/k4ngcaribug/vendor_extra -b ten vendor/extra
 git clone --depth=1 https://github.com/k4ngcaribug/vendor_extra -b dolby vendor/dolby/ds

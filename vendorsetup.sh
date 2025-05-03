a036823c16755a72fdde0cd3ec99191deefcf5c8
k4ngcaribug <neophyteplayground@gmail.com>
2025-05-03 16:56:31 +0000

    vince: Add vendorsetup

---
+++ vendorsetup.sh
@@ -0,0 +1,7 @@
+git clone --depth=1 https://github.com/vince-labs/vendor_xiaomi_vince -b 13 vendor/xiaomi/vince
+git clone --depth=1 https://github.com/vince-labs/kernel_xiaomi_vince -b main kernel/xiaomi/vince
+git clone --depth=1 https://github.com/k4ngcaribug/hardware_xiaomi -b 13 hardware/xiaomi
+git clone --depth=1 https://github.com/k4ngcaribug/vendor_extra -b ten vendor/extra
+git clone --depth=1 https://github.com/k4ngcaribug/vendor_extra -b dolby vendor/dolby/ds
+git clone --depth=1 https://github.com/GrapheneOS/platform_external_Camera -b 13 platform/external/Camera
+git clone --depth=1 https://github.com/iusmac/RevampedFMRadio -b qcom packages/apps/RevampedFMRadio

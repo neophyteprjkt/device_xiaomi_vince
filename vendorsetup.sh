aa31368c42cfbfbed593acb73364a45b5d3cd219
neophyte <neophyteplayground@gmail.com>
2025-12-25 06:45:16 +0100

    vince: Add vendorsetup

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+git clone --depth=1 https://github.com/neophyteprjkt/vendor_xiaomi_vince -b supex vendor/xiaomi/vince
+git clone --depth=1 https://github.com/Project-Nightcord/kernel_msm-4.9 kernel/xiaomi/vince
+git clone --depth=1 https://github.com/GrapheneOS/platform_external_Camera -b 13 platform/external/Camera
+git clone --depth=1 https://github.com/iusmac/RevampedFMRadio -b qcom packages/apps/RevampedFMRadio
+git clone --depth=1 https://github.com/neophyteprjkt/vendor_extra -b 14 vendor/lineage-priv
